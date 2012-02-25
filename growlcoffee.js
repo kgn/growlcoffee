b8a0053d46ee99c547d082bad8353d6610fa115f
David Keegan <git@davidkeegan.com>
2012-02-24 22:07:53 -0800

    updating js

--- growlcoffee.js
+++ growlcoffee.js
@@ -1,20 +1,18 @@
 (function() {
-  var coffee, exec, filename, growl;
+  var basename, coffee, exec, growl;
+  basename = require('path').basename;
   exec = require('child_process').exec;
   coffee = require('coffee-script');
-  filename = function(filepath) {
-    return filepath.split('/').slice(-1)[0];
-  };
   growl = function(title, message, imagename) {
     return exec("growlnotify '" + title + "' -n 'CoffeeScript' -m '" + message + "' --image '" + __dirname + "/" + imagename + ".png'");
   };
   coffee.on('failure', function(error, task) {
     var issue;
     issue = error.message.split(',')[1].trim();
-    growl(issue, filename(task.file), 'failure');
+    growl(issue, basename(task.file), 'failure');
     return console.error(error.message);
   });
   coffee.on('success', function(task) {
-    return growl('Compiled', filename(task.file), 'success');
+    return growl('Compiled', basename(task.file), 'success');
   });
 }).call(this);
